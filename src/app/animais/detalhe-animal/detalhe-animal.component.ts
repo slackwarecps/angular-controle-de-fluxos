efb4e12b28f14553e8e7986d54f8bc4c929f6714
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-05 08:30:32 -0300

    servico activated Route

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Animal } from '../animais';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-detalhe-animal',
   templateUrl: './detalhe-animal.component.html',
-  styleUrls: ['./detalhe-animal.component.css']
+  styleUrls: ['./detalhe-animal.component.css'],
 })
 export class DetalheAnimalComponent implements OnInit {
+  animalid!: number;
+  animal$!: Observable<Animal>;
 
-  constructor() { }
+  constructor(private animaisService: AnimaisService, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.animalid = this.activatedRoute.snapshot.params.animalid;
+    this.animal$ = this.animaisService.buscaPorId(this.animalid);
   }
-
 }
